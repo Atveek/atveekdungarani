5fbdeef49a4702f193b9bd5d8e9bced55d32b268
Atveek <atveekdungarani2021@gmail.com>
2023-10-02 19:54:13 +0530

    add favicon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,6 @@ const initialstate = {
 };
 export default function Contact() {
   const apiUrl = process.env.REACT_APP_API_URL;
-  const apiKey = process.env.REACT_APP_API_KEY;
   const [user, setUser] = useState(initialstate);
   let name, value;
   const handleChange = (e) => {
@@ -19,7 +18,7 @@ export default function Contact() {
   const submit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch(apiUrl + apiKey, {
+    const res = await fetch(apiUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
